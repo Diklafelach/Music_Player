05118244a045a07ce7328cde7cceb44dc5d8895e
dikla felach <81649744+Diklafelach@users.noreply.github.com>
2022-08-23 16:07:45 +0300

    adding the GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Music_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(348, 126);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(112, 34);
+            this.button.TabIndex = 0;
+            this.button.Text = "button1";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            this.button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button_KeyPress);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(326, 89);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(150, 31);
+            this.Search.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(64, 85);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(256, 187);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        public System.Windows.Forms.TextBox Search;
+        public System.Windows.Forms.PictureBox pictureBox;
+       
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using Music_Player.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Music_Player
+{
+    public partial class Form1 : Form
+    {
+        Music[] songs = new Music[] { new Pop(), new Rock() };
+        SoundPlayer foo;
+        static int count=0;
+        string preText;
+        public Form1()
+        {
+            preText = " ";
+            InitializeComponent();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            string url;
+            
+            if(!(preText.Equals(Search.Text)))
+            {
+                preText = Search.Text;
+                count = 0;
+            }
+            if (preText == "Pop" && count==0)
+            {
+                url = songs[0].UrlSong();
+                foo= new SoundPlayer(@url);
+                foo.SoundLocation = url;
+                foo.Play();
+                url = songs[0].UrlPic();
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.Load(url);
+            }
+            if(preText=="Rock" && count==0)
+            {
+                url = songs[1].UrlSong();
+                foo = new SoundPlayer(@url);
+                foo.SoundLocation = url;
+                foo.Play();
+                url = songs[1].UrlPic();
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.Load(url);
+            }
+            count = 0;
+        }
+        private void button_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals(' ') && count!=1)// ' '== space
+            {
+                count=1;
+                if (Search.Text == "Pop")
+                {
+                    foo.Stop();
+                    return;
+                }
+                if(Search.Text=="Rock")
+                {
+                    foo.Stop();
+                    return;
+                }
+            }
+            else
+            {
+                count = 0;
+                foo.Play();
+            }
+        }
+
+       
+    }
+}
